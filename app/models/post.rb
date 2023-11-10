3d25426e7d15712ebb04776a42d45995b335e764
May Pyone <maypyone015@gmail.com>
2023-11-10 21:03:03 +0630

    Implement methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,21 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :likes
-    has_many :comments
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_posts_counter
+    author.posts_counter
+  end
+
+  def comments_counter
+    update(comment_counter: comments.count)
+  end
+
+  def likes_counter
+    update(like_counter: likes.count)
+  end
 end
-  
