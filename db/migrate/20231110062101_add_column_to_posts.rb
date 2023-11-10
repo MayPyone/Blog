7e32545fd34b5e2f53edc727da22a61e5e3a154a
May Pyone <maypyone015@gmail.com>
2023-11-10 13:08:44 +0630

    Add foreign keys

---
+++ db/migrate/20231110062101_add_column_to_posts.rb
@@ -0,0 +1,8 @@
+class AddColumnToPosts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :posts, :title, :string
+    add_column :posts, :text, :string
+    add_column :posts, :comment_counter, :integer
+    add_column :posts, :like_counter, :integer
+  end
+end
