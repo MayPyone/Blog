b737f589d917a73e5764d20d93d6c3849542aac0
May Pyone <maypyone015@gmail.com>
2023-11-16 15:13:56 +0630

    Remove commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # @posts = @user.posts
     @bio = @user.bio
     @recent_post = @user.recent_posts
   end
