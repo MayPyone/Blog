08f35ed269afc2b628a16d2a62099b1c6690468e
May Pyone <maypyone015@gmail.com>
2023-11-25 14:21:06 +0630

    Update tests for user show page

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :feature do
     before do
-    @user1 = User.create!(id: 5, name: 'Bijay',photo: 'https://c0.klipartz.com/pngpicture/695/991/gratis-png-profesor.png') 
-    @user2 = User.create!(id: 6, name: 'May', photo: 'https://c0.klipartz.com/pngpicture/695/991/gratis-png-profesor.png') 
+    @user1 = User.create!(id: 5, name: 'Bijay',photo: 'https://c0.klipartz.com/pngpicture/695/991/gratis-png-profesor.png', bio: 'officer') 
+    @user2 = User.create!(id: 6, name: 'May', photo: 'https://c0.klipartz.com/pngpicture/695/991/gratis-png-profesor.png', bio: 'student') 
+    Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user1)
+    Post.create!(title: 'Intro1', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user1)
     Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user1)
     Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user1)
-    Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user2)
+   
+    @post1=Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user2)
     
     end
 
@@ -19,26 +22,53 @@ RSpec.describe 'Users', type: :feature do
       expect(page).to have_content("May")
     end
 
-    it "returns users' image link" do
+    it "returns users' profile" do
       visit user_path(@user1)
       expect(page).to have_css("img[src*='#{@user1.photo}']")
       visit user_path(@user2)
       expect(page).to have_css("img[src*='#{@user2.photo}']")
     end
-
+    it "return recent 3 posts" do
+        visit user_path(@user1)
+      expect(@user1.recent_posts.count).to have_content(3)
+     visit user_path(@user2)
+    expect(@user2.posts.count).to have_content(1)
+     end
+ 
     it "return number of posts" do
        visit user_path(@user1)
-     expect(@user1.posts.count).to have_content(2)
+     expect(@user1.posts.count).to have_content(4)
     visit user_path(@user2)
    expect(@user2.posts.count).to have_content(1)
     end
 
-    it 'redirects to user show page' do
+    it "return user's bio" do
+        visit user_path(@user1)
+      expect(@user1.bio).to have_content('officer')
+     visit user_path(@user2)
+    expect(@user2.bio).to have_content('student')
+     end
+
+     it "return a button that let see all posts" do
+        visit user_path(@user1)
+        expect(page).to have_selector('button', text: 'See all posts')
+    
+     end
+
+    it 'redirects to user show page too see all posts' do
      visit user_path(@user1)
        save_and_open_page
        click_button("See all posts")
        sleep 15
       expect(current_path).to eq(user_posts_path(@user1)) 
+  end
+
+  it 'redirect to sepcific post when the user click the post' do
+    visit user_path(@user2)
+    save_and_open_page
+    click_link "Post #1"
+    sleep 15
+    expect(current_path).to eq(user_post_path(@user2,@post1)) 
   end
 end
 end
