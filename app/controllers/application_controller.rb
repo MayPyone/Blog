c14ca5781c2e231ca3a577fc4655803093eb135a
May Pyone <maypyone015@gmail.com>
2023-11-28 21:37:58 +0630

    Update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit({ roles: [] }, :email, :password, :name, :posts_counter)
+      user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :name, :posts_counter)
     end
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit({ roles: [] }, :email, :name, :password, :current_password)
+      user_params.permit({ roles: [] }, :email, :name, :password, :password_confirmation, :current_password)
     end
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
       user_params.permit(:name, :email)
